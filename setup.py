0327d4a27a569aa065bec5f1ead39c924f11b50c
Mikuláš Poul <git@mikulaspoul.cz>
2023-09-19 11:33:12 +0200

    Upgrade package versions, remove unsupported

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ EXTRA_REQUIRES = {
     "webhook": ["requests>=2.10.0"],
     "html": ["lxml>=4.4.0"],
     "crypto": ["cryptography>=2.0.0"],
-    "django": ["django-ipware>=2.0.0", "django>=1.11"],
+    "django": ["django-ipware>=2.0.0", "django>=4.2"],
 }
 
 ALL_REQUIRE = list(chain(*EXTRA_REQUIRES.values()))
@@ -27,12 +27,12 @@ EXTRA_REQUIRES["all"] = ALL_REQUIRE
 
 setup(
     name="pytracking2",
-    version="0.4.5",
+    version="0.4.6",
     description="Email open and click tracking",
     long_description=long_description,
-    url="https://github.com/mikicz/pytracking",
+    url="https://github.com/xelixdev/pytracking",
     author="Mikuláš Poul",
-    author_email="mikulaspoul@gmail.com",
+    author_email="mikulas.poul@xelix.com",
     license="New BSD",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -42,14 +42,14 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     keywords="email open click tracking",
     packages=find_packages(".", include=("pytracking", "pytracking.*")),
     extras_require=EXTRA_REQUIRES,
-    python_requires=">=3.7",
+    python_requires='>=3.8',
 )
